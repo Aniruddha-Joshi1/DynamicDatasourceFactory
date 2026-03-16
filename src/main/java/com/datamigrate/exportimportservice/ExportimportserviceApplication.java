8403be837e3dfb76675e1cd0cecff62a863a814f
AniruddhaJoshi <aniruddha.joshi@kanerika.com>
2026-03-16 12:04:01 +0530

    First commit

---
+++ src/main/java/com/datamigrate/exportimportservice/ExportimportserviceApplication.java
@@ -0,0 +1,13 @@
+package com.datamigrate.exportimportservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExportimportserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExportimportserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datamigrate/exportimportservice/ExportimportserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datamigrate.exportimportservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExportimportserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
